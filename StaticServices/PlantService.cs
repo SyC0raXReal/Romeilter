7e9987414ed2d40feca1228b5d798785573a7b88
Martin <hahner.m@tutanota.com>
2024-12-22 22:53:28 +0100

    Add more plants and some recipes

--- StaticServices/PlantService.cs
+++ StaticServices/PlantService.cs
@@ -3,14 +3,72 @@ using Romeilter.Models;
 namespace Romeilter.StaticServices;
 public static class PlantService
 {
+    public static AlchemyPlantModel Water = new()
+    {
+        Name = "Wasser",
+        Description = "Klares reines Brunnenwasser",
+        Effect = "Verstärkt die Pflanzlichen Effekte bei Trankherstellung",
+        Occurance = "weltweit",
+        Positive = true,
+        Rarity = Rarity.Common,
+    };
+
+    public static AlchemyPlantModel Alcohol = new()
+    {
+        Name = "Alkohol",
+        Description = "Hochprozentiger",
+        Effect = "Lässt die Planzlichen Effekte verdunsten wodurch diese durch Atmung aufgenommen werden können.",
+        Occurance = "weltweit",
+        Positive = true,
+        Rarity = Rarity.Common,
+    };
+
+    public static AlchemyPlantModel Schimmerwurz = new()
+    {
+        Name = "Schimmerwurz",
+        Description = "Eine kleine leuchtendes Gewächs mit dunkelblauen Blättern und einer großen schimmernden Wurzelknolle",
+        Effect = "Lässt Objekte und Lebewesen für 1 h leuchten",
+        Occurance = "Kann weltweit in feuchten und dunklen Orten gefunden werden",
+        Positive = true,
+        Rarity = Rarity.Common,
+    };
+
+    public static AlchemyPlantModel Mate = new()
+    {
+        Name = "Mate",
+        Description = "Ein kleines unscheinbares gewächs, mit dunkelgrünen kleinen herzgeförmten Blättern",
+        Effect = "Man fühlt sich gestärkt und hat für 1 Minute +5 Boni auf Handeln",
+        Occurance = "In Cherstans Trockensteppen",
+        Positive = true,
+        Rarity = Rarity.Uncommon,
+    };
+
+    public static AlchemyPlantModel Blutwurz = new()
+    {
+        Name = "Mate",
+        Description = "Die Blutrote Wurzel des gelben Enzian, hat einen sehr bitteren geschmack.",
+        Effect = "Man fühlt sich geschwächt und wird von einer starken Übelkeit übernommen. Bei Hautkontakt kommt es zu Rötungen und Juckreitz. -5 Malus auf Handeln, Konzentration",
+        Occurance = "In den Sümpfen im Norden Cherstans / Südosten von Ahian",
+        Positive = false,
+        Rarity = Rarity.Uncommon,
+    };
+
+    public static AlchemyPlantModel Tzabra = new()
+    {
+        Name = "Tzabra",
+        Description = "Auffallend rote längliche Kaktusfurcht.",
+        Effect = "Löst bei verzehr kurzzeitig starke Glückgefühle aus. Bei übermäßigen verzehr betäubt es die Nerven und verlangsamt den Geist.",
+        Occurance = "In Cherstans Trockensteppen",
+        Positive = false,
+        Rarity = Rarity.Common,
+    };
+
     public static HashSet<AlchemyPlantModel> AlchemyIngredients = [
-        new (){
-            Name = "Schimmerwurz",
-            Description = "Eine kleine leuchtendes Gewächs mit dunkelblauen Blättern und einer großen schimmernden Wurzelknolle",
-            Effect = "Lässt Objekte und Lebewesen für 1 h leuchten",
-            Occurance = "Kann weltweit in feuchten und dunklen Orten gefunden werden",
-            Positive = true,
-            Rarity = Rarity.Common,
-        },
+        Water,
+        Alcohol,
+        Schimmerwurz,
+        Mate,
+        Blutwurz,
+        Tzabra,
     ];
 }
--- StaticServices/RecipeService.cs
+++ StaticServices/RecipeService.cs
@@ -4,5 +4,30 @@ namespace Romeilter.StaticServices;
 public static class RecipeService
 {
     public static HashSet<AlchemyRecipeModel> AlchemyRecipes = [
+        new () {
+            Name = "Stärketrank",
+            Description = "Stärkt den Körper für kurze Zeit",
+            Effect = "(Fertigkeit) Boni auf Handeln für 1 Minute",
+            FirstIngredient = PlantService.Mate,
+            SecondIngredient = PlantService.Water,
+            Grenade = false,
+            Postive = true,
+            Rarity = Rarity.Uncommon,
+            Cost = 5,
+            Currency = Currency.Silver,
+        },
+        new () {
+            Name = "Schwächungswurftrank",
+            Description = "Stärkt den Körper für kurze Zeit",
+            Effect = "(Fertigkeit + Wasseranteil) Boni auf Handeln für 1 Minute, verdunstungsbereich (Fertigkeit + Alkoholanteil)",
+            FirstIngredient = PlantService.Blutwurz,
+            SecondIngredient = PlantService.Water,
+            ThirdIngredient = PlantService.Alcohol,
+            Grenade = true,
+            Postive = false,
+            Rarity = Rarity.Uncommon,
+            Cost = 7,
+            Currency = Currency.Silver,
+        }
     ];
 }
